fc09d92b430775823b02485a82138a8fbefd2f7d
Harsha Shetty <harsha.shetty@siemens-healthineers.com>
2023-06-24 20:41:35 +0530

    basics of express and node

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,6 @@ module.exports = {
         "ecmaVersion": "latest"
     },
     "rules": {
+        "comma-dangle": 0
     }
 }
--- app.js
+++ app.js
@@ -1,17 +1,49 @@
-const express = require('express'); // ES6 based import
-
+const express = require('express'); // Common-JS based import
 const app = express();
 
-const port = 8080; // port to serve the requests.
+const chalk = require('chalk'); // helps in colouring the text => use version 4.1 to work with Common-JS
+const morgan = require('morgan'); // morgan-package gives insights about the web-requests.
+const path = require('path'); // for path related tasks - no need to install this package(comes by default)
+const debug = require('debug')('app'); // setting debug to 'app' level scope.
+
+const bookRouter = express.Router();
+const port = process.env.PORT; // port to serve the requests.
+
+//const db = mongoose.connect('mongodb+srv://harshashetty67:dHs8ttYwb2hvf7Fr@my-mongo.elbhwd6.mongodb.net/');
+
 
 function basicGet(req, res) {
 	res.send('Start with basic get call..');
 }
 
-app.get('/', basicGet)
+app.use(morgan('tiny')); // 'tiny' gives short-info , if you pass 'combined' you get detailed info.
+
+/*
+- Path to the index.html file => using express.static() helper to render static html page. We can given the path directly.
+_ The static html page helps in loading static content, without restarting node server app.
+*/
+app.use(express.static(path.join(__dirname, '/public/')));
+
+
+// If static we page is not present then this basic get() callback is invoked.
+// the callback response gets ignored, if express finds index.html page
+// all app.use() should be assigned before api call.
+
+app.get('/', basicGet);
+
+/*bookRouter.route('/books').get((req, res) => { 
+	res.json({ hello: "my json result" });
+}
+)*/
+
+//app.use('/api', bookRouter);  // attaching the router object.
 
 app.listen(port, () => {
-	console.log(`port is {process.env.PORT} and coonection string is {process.env.ConnectionString}`);
+	console.log(`Server is running on port:${chalk.green(port)}`);  // port is passed as env variable from nodemonConig block in package.json
+	debug('This is a debug message and it can only be seen in DEBUG mode.');
+	/* 	 - We can see debug message when we run => 'DEBUG=app node <file-name>.js' 
+			 - This command works in Linux- only 
+	*/
 });
 
 
